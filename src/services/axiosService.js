c08919ae3f6884fa40022631d3831fc6c7c3bd39
Piter Golius <piter.golius@gmail.com>
2022-08-08 19:02:56 +0300

    add token to req

--- src/services/axiosService.js
+++ src/services/axiosService.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useAuth } from "../hooks/useAuth";
 
 const REACT_APP_BASE_PATH =
   process.env.REACT_APP_BASE_PATH || "http://localhost:8000/api/";
@@ -8,5 +9,11 @@ const instance = axios.create({
   baseURL: REACT_APP_BASE_PATH,
   timeout: timeout
 });
-
+instance.interceptors.request.use(config => {
+  const { token } = useAuth();
+  if (token) {
+    config.headers.Authorization = "Bearer " + token;
+  }
+  return config;
+});
 export default instance;
